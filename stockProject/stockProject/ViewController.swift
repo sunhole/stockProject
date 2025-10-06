48d2427032f4cb710aff70c4cb6c41f772480ee3
sun <dkdlshs123@naver.com>
2025-10-06 21:01:38 +0900

    init
    update: init  프로젝트 초기 설정 및 깃 이그노어 처리

---
+++ stockProject/stockProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockProject
+//
+//  Created by vision on 10/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ stockProject/stockProjectTests/stockProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  stockProjectTests.swift
+//  stockProjectTests
+//
+//  Created by vision on 10/6/25.
+//
+
+import Testing
+@testable import stockProject
+
+struct stockProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
